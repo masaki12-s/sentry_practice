e5ff69c6183ca7855f3e9297f8ba2e709563c97a
masaki12-s <asam.archer1112@yahoo.ne.jp>
2023-03-07 15:13:51 +0900

    [fix] node用の実装に変更

--- sentry_sdk.js
+++ sentry_sdk.js
@@ -1,12 +1,31 @@
-import * as Sentry from "@sentry/browser";
-import { BrowserTracing } from "@sentry/tracing";
+const Sentry = require("@sentry/node");
+// or use es6 import statements
+// import * as Sentry from '@sentry/node';
+
+const Tracing = require("@sentry/tracing");
+// or use es6 import statements
+// import * as Tracing from '@sentry/tracing';
 
 Sentry.init({
   dsn: "http://08b899dcebcf4dd7828340363e5d9524@localhost:9000/1",
-  integrations: [new BrowserTracing()],
 
   // Set tracesSampleRate to 1.0 to capture 100%
   // of transactions for performance monitoring.
   // We recommend adjusting this value in production
   tracesSampleRate: 1.0,
 });
+
+const transaction = Sentry.startTransaction({
+  op: "test",
+  name: "My First Test Transaction",
+});
+
+setTimeout(() => {
+  try {
+    foo();
+  } catch (e) {
+    Sentry.captureException(e);
+  } finally {
+    transaction.finish();
+  }
+}, 99);
